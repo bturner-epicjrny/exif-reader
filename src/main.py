5c1f4e170ea2cbcb26919a1d463c8da2114d2e3e
Brian Turner <brian@epicjrny.com>
2024-10-12 08:19:08 -0700

    Add filename command-line argument.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,22 @@ import argparse
 
 
 def main():
-  print("Hello World!")
+  parser = argparse.ArgumentParser(
+    prog='EXIF Reader',
+    description='Read EXIF metadata from image/sidecar file and output it to the console.',
+  )
+  # Add positional parameter for path to image file/sidecar file.
+  parser.add_argument(
+    'filename', 
+    help='Path to image file.')
+  
+  args = parser.parse_args()
+
+
+  print(f"Processing file: {args.filename}...")
+
+
+
 
 
 if __name__ == '__main__':
